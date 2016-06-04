8e3d32628906dc495d3c73fc44d862604d0e7beb
Aangi <aashah.6198@gmail.com>
2016-06-03 21:23:55 -0400

    added project page

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,15 +9,16 @@
     paragraphElements[0] = document.getElementById("paragraphOne");
     paragraphElements[1] = document.getElementById("paragraphTwo");
     paragraphElements[2] = document.getElementById("paragraphThree");
+    paragraphElements[3] = document.getElementById("paragraphFour");
     
     // define your paragraphs array;
     var paragraphs = [];
     
     // data for my pages
-    paragraphs[0] = "Hello I am Nidhi Thakkar.I am studying Software Engeering Technology at Centennial College progress campus ";
-    paragraphs[1] = "My mission is to  build a network of companies related to software, which will contribute to the development and wide spread usage of the company's software packages..";
-    paragraphs[2] = "for this mission i have come to canada leaving back my home country";
-    
+    paragraphs[0] = "Hello I am Nidhi Thakkar.I am studying Software Engeering Technology at Centennial College progress campus.My mission is to  build a network of companies related to software, which will contribute to the development and wide spread usage of the company's software packages..for this mission i have come to canada leaving back my home country ";
+    paragraphs[1] = "My hobby is to play volleyball.In my school days i played a lot.I even got a change to play in the inter school competition.However, thesedays i do not get time to play but would continue in future.My favourite volleyball player is Jimmy George. He is the first Indian volleyball player to become a professional and played club volleyball in Italy  ";
+    paragraphs[2] = "Secondly i am fond of travelling .Travelling helps me connect with new people, which helps to improve my interpersonal skills.For a year full of work, projects, ratings, traffic, tensions travel is like a fresh dose of much needed oxygen!";
+    paragraphs[3]="I am too much fond of doing volunteer service.In my hometown there were too many people who did not even get meal.This was the reason i joined a campaign called change vadodara campaign esspecially to educate children and provide proper food to needy people.I did this work with full love and devotion"
     // second way to define an array
     //var paragraphs = new Array();
     
